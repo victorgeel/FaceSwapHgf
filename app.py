6360ee486e2a75ad7fa1225132daf8453be2925d
victor geek <victorisgeek@users.noreply.huggingface.co>
2024-08-07 07:00:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -903,4 +903,4 @@ if __name__ == "__main__":
     if USE_COLAB:
         print("Running in colab mode")
 
-    interface.queue(concurrency_count=2, max_size=20).launch(share=True)
+    interface.queue(concurrency_count=2, max_size=20).launch(share=False)
