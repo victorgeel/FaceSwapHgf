53aa61db2fc42b264ca44ac4148c77f61f5a2597
victor geek <victorisgeek@users.noreply.huggingface.co>
2024-09-11 04:11:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -543,7 +543,7 @@ footer{display:none !important}
 """
 
 with gr.Blocks(css=css) as interface:
-    gr.Markdown("# 🦋 FaceSwap with Enhnacer 🦋")
+    gr.Markdown("# 🦋FaceSwap & Enhnacer🦋")
     with gr.Row():
         with gr.Row():
             with gr.Column(scale=0.4):
